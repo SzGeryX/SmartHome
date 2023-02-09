c391db0a9409ee049bb8c703b5352aef8670d9f7
GeryX <gszentjobi@gmail.com>
2023-02-09 10:50:17 +0100

    probably final form

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function showHidden1() {
+    document.getElementById("hidden1").style.visibility = "visible"
+}
+
+function hideHidden1() {
+    document.getElementById("hidden1").style.visibility = "hidden"
+}
+
+function showHidden2() {
+    document.getElementById("hidden2").style.visibility = "visible"
+}
+
+function hideHidden2() {
+    document.getElementById("hidden2").style.visibility = "hidden"
+}
